e2fe998f51d3d4dad7dd7e716c5b937618e886fb
RAED <rrrr53@hotmail.com>
2022-07-02 13:04:27 +0300

    [setup.py] Update

--- setup.py
+++ setup.py
@@ -12,5 +12,4 @@ setup(name='enigma2-plugin-extensions-BackUpFlash',
        packages=[PLUGIN_DIR],
        package_dir={PLUGIN_DIR: 'usr'},
        package_data={PLUGIN_DIR: ['plugin.png', '*/*.png']},
-       package_data={PLUGIN_DIR: ['*.png', 'buttons/*.png', 'locale/*/LC_MESSAGES/*.mo']},
       )
