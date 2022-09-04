0c9ab70c6d5d0ec29d99d18c44f39bc5e44ccef9
RAED <rrrr53@hotmail.com>
2022-09-04 12:56:01 +0300

    Add BlackHole PY3 image Download links

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ##setup command=wget https://raw.githubusercontent.com/fairbird/BackUpFlash/main/installer.sh -O - | /bin/sh
 
-version=6.9
-description=What is NEW :\n-Update OpenPLI-Unoffical images Url download\n-Fix Update online \n\n**************************\n: ما هو الجديد\n-OpenPLI-Unoffical تغير رابط تحميل صور\n-تعديل رابط تحميل صور
+version=7.0
+description=What is NEW :\n-Add BlackHole PY3 image Download links\n-Fix Update online \n\n**************************\n: ما هو الجديد\n-BlackHole Py3 إضافة رابط تحميل صور
 
 echo ""
 if [ ! -d '/usr/lib64' ]; then
