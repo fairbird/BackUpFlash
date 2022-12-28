1d90f3a5eef2eb8a5c826969aec7c019c43df840
RAED <rrrr53@hotmail.com>
2022-12-28 16:15:35 +0300

    Update backup.py

--- usr/lib/enigma2/python/Plugins/Extensions/backupflashe/tools/backup.py
+++ usr/lib/enigma2/python/Plugins/Extensions/backupflashe/tools/backup.py
@@ -1,65 +1,65 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# RAED & mfaraj57 &  (c) 2018
+# RAED & mfaraj57 (c) 2018
 # Code RAED & mfaraj57
 
-# python3
-from __future__ import print_function
-
-from Components.ActionMap import ActionMap
 from Components.Label import Label
+from Components.config import config
+from Components.ActionMap import ActionMap
+from Components.MenuList import MenuList
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
-from Components.MenuList import MenuList
 from Tools.Directories import fileExists
-from Components.config import config
 from enigma import eTimer
-import datetime
-import os
+import os, datetime
 
 from .Console import Console
 from .progress import ProgressScreen 
-from .bftools import logdata, getboxtype, dellog, getimage_name, copylog
-
+from .bftools import logdata, dellog, copylog
+        
 BRANDOS = '/var/lib/dpkg/status' ## DreamOS
 BAINIT = '/sbin/bainit'
 
+
 class doBackUp(Screen):
-    def __init__(self, session, device_path = None, image_formats = None, image_compression_value = 0):
+
+    def __init__(self, session, image_name = None, device_path = None, image_formats = None, image_compression_value = 0):
         Screen.__init__(self, session)
         self.list = []
         self['key_red'] = Label(_('Cancel'))
         self['key_green'] = Label(_('Backup'))
         self['lab1'] = Label('')
         self['actions'] = ActionMap(['WizardActions', 'ColorActions'],
-        	{
-        		'red': self.close,
-         		'green': self.doBackUp,
-         		'back': self.close
-         	})
+                {
+                        'red': self.close,
+                        'green': self.doBackUpjob,
+                        'back': self.close
+                })
+        self.image_name = image_name
         self.device_path = device_path
         self.image_formats = image_formats
         self.image_compression_value = image_compression_value
         self.deviceok = True
         self.timer = eTimer()
         try:
-            self.timer.callback.append(self.doBackUp)
+            self.timer.callback.append(self.doBackUpjob)
         except:
-            self.timer_conn = self.timer.timeout.connect(self.doBackUp)
+            self.timer_conn = self.timer.timeout.connect(self.doBackUpjob)
         self.timer.start(6, 1)
 
-    def doBackUp(self):
+    def doBackUpjob(self):
+        if fileExists("/tmp/root"):
+             os.system('umount /tmp/root >> %s 2>&1')
+        if not fileExists("/tmp/root/usr"):
+             os.system("rm -r /tmp/root")
         if fileExists("/tmp/.cancelBackup"):
              os.system("rm -f /tmp/.cancelBackup")
         self.timer.stop()
         cmdlist = []
-        boxtype = getboxtype()
-        getname = getimage_name()
+        NOW = datetime.datetime.now()
         LOG = '/tmp/backupflash.scr'
-        now = datetime.datetime.now()
-        DATETIME = now.strftime('%Y-%m-%d-%H-%M')
         logdata("Backup log","start")
-        logdata("Start Time",now.strftime('%H:%M')) ## Print Start time to log file
+        logdata("Start Time", NOW.strftime('%H:%M')) ## Print Start time to log file
         if config.backupflashe.cleanba.value:
              if fileExists(BAINIT):
                   if fileExists(BRANDOS):
@@ -73,24 +73,24 @@ class doBackUp(Screen):
         if os.path.exists("/etc/init.d/openvpn"):
             os.system('/etc/init.d/openvpn stop >> >> %s' % LOG)
         if self.image_formats == 'xz':
-            IMAGENAME = '%s-%s-%s.tar.xz ' % (getname, boxtype, DATETIME)
-            IMAGENAME1 = os.path.join(self.device_path, IMAGENAME)
-            self.IMAGENAME1=IMAGENAME1
+            IMAGENAME = '%s.tar.xz' % self.image_name
+            IMAGENAMEPATH = os.path.join(self.device_path, IMAGENAME)
+            self.IMAGENAMEPATH = IMAGENAMEPATH
             compression_value=self.image_compression_value
             if fileExists(BRANDOS):
-               COMMANDTAR = 'tar --exclude=smg.sock --exclude msg.sock -cf - -C /tmp/root . | xz -%s -T 0 -c - > %s' % (compression_value, IMAGENAME1)
+               COMMANDTAR = 'tar --exclude=smg.sock --exclude msg.sock -cf - -C /tmp/root . | xz -%s -T 0 -c - > %s' % (compression_value, IMAGENAMEPATH)
             else:
-               COMMANDTAR = 'tar -cf - -C /tmp/root . | xz -%s -T 0 -c - > %s' % (compression_value, IMAGENAME1)
+               COMMANDTAR = 'tar -cf - -C /tmp/root . | xz -%s -T 0 -c - > %s' % (compression_value, IMAGENAMEPATH)
         else:
-            IMAGENAME = '%s-%s-%s.tar.gz ' % (getname, boxtype, DATETIME)
-            IMAGENAME1 = os.path.join(self.device_path, IMAGENAME)
-            compression_value=self.image_compression_value
+            IMAGENAME = '%s.tar.gz' % self.image_name
+            IMAGENAMEPATH = os.path.join(self.device_path, IMAGENAME)
+            compression_value = self.image_compression_value
             if fileExists(BRANDOS):
-               COMMANDTAR = 'tar -cf - --exclude=smg.sock --exclude msg.sock -C /tmp/root . | pigz -%s  > %s' % (compression_value, IMAGENAME1)
+               COMMANDTAR = 'tar -cf - --exclude=smg.sock --exclude msg.sock -C /tmp/root . | pigz -%s  > %s' % (compression_value, IMAGENAMEPATH)
             else:
-               COMMANDTAR = 'tar -cf - -C /tmp/root . | pigz -%s  > %s' % (compression_value, IMAGENAME1)
-        IMAGENAME1 = os.path.join(self.device_path, IMAGENAME)
-        self.IMAGENAME1=IMAGENAME1
+               COMMANDTAR = 'tar -cf - -C /tmp/root . | pigz -%s  > %s' % (compression_value, IMAGENAMEPATH)
+        IMAGENAMEPATH = os.path.join(self.device_path, IMAGENAME)
+        self.IMAGENAMEPATH = IMAGENAMEPATH
         if self.device_path:
             mytitle = _('Backup image')
             cmdlist.append('exec > /tmp/backupflash.scr')
@@ -103,15 +103,15 @@ class doBackUp(Screen):
             cmdlist.append(COMMANDTAR)
             cmdlist.append('umount /tmp/root >> %s' % LOG)
             cmdlist.append('rmdir /tmp/root >> %s' % LOG)
-            cmdlist.append('chmod 777 %s >> %s' % (IMAGENAME1, LOG))
+            cmdlist.append('chmod 777 %s >> %s' % (IMAGENAMEPATH, LOG))
             if os.path.exists("/etc/init.d/openvpn"):
                cmdlist.append('/etc/init.d/openvpn start >> %s' % LOG)
             for item in cmdlist:
                 logdata("command",str(item))
-            self.session.openWithCallback(self.cleanba, ProgressScreen, title=mytitle, cmdlist=cmdlist, imagePath=self.IMAGENAME1)
+            self.session.openWithCallback(self.cleanba, ProgressScreen, title = mytitle, cmdlist = cmdlist, imagePath = self.IMAGENAMEPATH)
         else:
             self.session.open(MessageBox, _('Sorry no device found to store backup.\nPlease check your media in devices manager.'), MessageBox.TYPE_INFO)
-        logdata("Finished Time",now.strftime('%H:%M')) ## Print Finished time to log file
+        logdata("Finished Time", NOW.strftime('%H:%M')) ## Print Finished time to log file
 
     def cleanba(self):
         if config.backupflashe.cleanba.value:
@@ -129,17 +129,18 @@ class doBackUp(Screen):
              self.dofinish()
 
     def dofinish(self):
-    	if fileExists("/tmp/.cancelBackup"):
-             os.system("rm -f /tmp/.cancelBackup")
+        cancelBackup = "/tmp/.cancelBackup"
+        if self.image_formats == 'xz':
+             IMAGENAME = '%s.tar.xz' % self.image_name
+             IMAGENAMEPATH = os.path.join(self.device_path, IMAGENAME)
+        else:
+             IMAGENAME = '%s.tar.gz' % self.image_name
+             IMAGENAMEPATH = os.path.join(self.device_path, IMAGENAME)
+        if fileExists(cancelBackup):
+             os.remove(cancelBackup)
+             os.remove(IMAGENAMEPATH)
+             logdata(".\n.\nCancelled Backup !!!!!!")
              self.close()
-    	else:
-             boxtype = getboxtype()
-             getname = getimage_name()
-             now = datetime.datetime.now()
-             DATETIME = now.strftime('%Y-%m-%d-%H-%M')
-             if self.image_formats == 'xz':
-                  IMAGENAME = '%s-%s-%s.tar.xz ' % (getname, boxtype, DATETIME)
-             else:
-                  IMAGENAME = '%s-%s-%s.tar.gz ' % (getname, boxtype, DATETIME)
+        else:
              self.session.open(MessageBox, _('(%s)\non\n[%s]\n\nfinished. Press (Exit) or (Ok) Button.' % (IMAGENAME, self.device_path)), MessageBox.TYPE_INFO)
              self.close()
