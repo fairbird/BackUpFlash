e28f96fb58172c9a0c99acb0b55a8a6e3354a821
RAED <rrrr53@hotmail.com>
2025-06-08 01:12:31 +0300

    Update bftools.py

--- usr/lib/enigma2/python/Plugins/Extensions/backupflashe/tools/bftools.py
+++ usr/lib/enigma2/python/Plugins/Extensions/backupflashe/tools/bftools.py
@@ -22,6 +22,11 @@ if os.path.isfile('/var/lib/dpkg/status'):
 else:
 	STATUS = '/var/lib/opkg/status'
 
+# Set headers (User-Agent is often required to avoid 403)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+
 #def logdata(label, txt):
 #    try:
 #        bfile=open(logfile, 'a')
@@ -139,23 +144,23 @@ def get_package():
 	return (XZ_, PIGZ_, ZIP_, WGET_)  # Always return a tuple
 
 
-def get_images(url, regx):
+def get_images(url,regx):
 	images = []
 	# Replace http with https to avoid redirects
 	if url.startswith('http://'):
 		url = url.replace('http://', 'https://', 1)
 	logdata("images_url",url)
 	try:
-		req = compat_Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-		response = compat_urlopen(req, timeout=5)
+		req = compat_Request(url, headers=headers) # # add headers to fix HTTP Error 403: Forbidden
+		response = compat_urlopen(req,timeout=5)
 		data = response.read()
 		response.close()
-		match = re.findall(regx, data, re.M | re.I)
-		for item1, item2 in match:
-			images.append((item1, item2))
+		match = re.findall(regx,data, re.M|re.I)
+		for item1,item2 in match:
+			images.append((item1,item2))
 		return images
-	except Exception as e:
-		print("Error fetching images:", str(e))  # Debug error
+	except:
+		trace_error()
 		return []
 
 
@@ -184,7 +189,7 @@ def get_images_mediafire(url):
 	logdata("images_url",url)
 	def readnet(url):
 		try:
-			req = compat_Request(url, headers={'User-Agent': 'Mozilla/5.0'}) # add [headers={'User-Agent': 'Mozilla/5.0'}] to fix HTTP Error 403: Forbidden
+			req = compat_Request(url, headers=headers) # add headers to fix HTTP Error 403: Forbidden
 			response = compat_urlopen(req,timeout=10)
 			if PY3:
 				data = response.read().decode('utf-8')
