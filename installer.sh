5b61d79ca2a4d37b752100a84642a8dcf8db254f
RAED <rrrr53@hotmail.com>
2025-07-22 19:39:58 +0300

    Bumb V11.5
    * Some fixes
    * Fix openatv download link

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##setup command=wget https://raw.githubusercontent.com/fairbird/BackUpFlash/main/installer.sh -O - | /bin/sh
 
-version=11.4
+version=11.5
 description=What is NEW :\n- Some fixes. -\n\n**************************\n: ما هو الجديد\n- بعض الإصلاحات
 
 echo ""
