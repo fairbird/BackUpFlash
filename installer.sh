caea403b4a505100cc95603ac02c719095587ec0
RAED <rrrr53@hotmail.com>
2025-10-21 19:04:20 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 ##setup command=wget https://raw.githubusercontent.com/fairbird/BackUpFlash/main/installer.sh -O - | /bin/sh
 
 version=12.1
-description=What is NEW :\n- Fix download images from mediafire \n\n**************************\n: ما هو الجديد\n- mediafire إضافة تحميل الصور من
+description=What is NEW :\n- Fix download images from mediafire \n\n**************************\n: ما هو الجديد\n- mediafire إصلاح تحميل الصور من
 
 echo ""
 if [ ! -d '/usr/lib64' ]; then
